5942004e784af5eb82f724b5d0204dbf349aa739
vishalsinghhh <vishalsingh0710vs@gmail.com>
2022-07-28 13:46:23 +0530

    all done

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,82 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+    string public name = "Decentragram";
+
+    // Store Posts
+    uint256 public imageCount = 0;
+    mapping(uint256 => Image) public images;
+
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event ImageTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    // Create Posts
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        // Make sure image description exists
+        require(bytes(_imgHash).length > 0);
+        require(bytes(_description).length > 0);
+        require(msg.sender != address(0x0));
+
+        // Increment imagr id
+        imageCount++;
+
+        // Add Image to contract
+        images[imageCount] = Image(
+            imageCount,
+            _imgHash,
+            _description,
+            0,
+            msg.sender
+        );
+
+        // Trigger an event
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+
+    // Tip Images
+    function tipImageOwner(uint256 _id) public payable {
+        // Make sure the id is valid
+        require(_id > 0 && _id <= imageCount);
+        // Fetch the image
+        Image memory _image = images[_id];
+        // Ftech the author
+        address payable _author = _image.author;
+        // Pay the author by sending them Ether
+        address(_author).transfer(msg.value);
+        // Increment the tip amount
+        _image.tipAmount = _image.tipAmount + msg.value;
+        // Update the image
+        images[_id] = _image;
+        // Trigger an event
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            _image.tipAmount,
+            _author
+        );
+    }
 }
